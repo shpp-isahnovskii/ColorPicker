8adbf4e99f39b73b09cd0f3db6c9a24530fc01d9
Ivan <yaivan13@gmail.com>
2020-05-05 17:51:20 +0300

    add 2 untracked files

---
+++ src/ColorPicker.ts
@@ -0,0 +1,89 @@
+import { PickerInterface } from '@/interface/PickerInterface';
+
+export class ColorPicker {
+  private gradientBox: HTMLElement = document.createElement('div');
+  private picker: HTMLElement = document.createElement('div');
+  private pickerPos: {x: number, y: number} = {x: 0, y: 0};
+  private pickerRadius: number = 0;
+
+  constructor(picker: PickerInterface) {
+    this.gradientBox.appendChild(this.picker);
+    this.setId(picker);
+    this.setClass(picker);
+    this.setClickEvent(this.gradientBox,);
+  }
+
+  /**
+   * Initialisation. Add created dom elements to some element.
+   * @param name ID name of the parent "wrapper" element. Picker will be added inside this element
+   */
+  set joinTo(name: string) {
+    const element = document.getElementById(name);
+    if (element === null) {
+      return;
+    }
+    element.appendChild(this.gradientBox);
+    this.pickerRadius = this.picker.offsetWidth / 2;
+  }
+
+  /**
+   * Add id to the created DOM elements. Here is Rect and picker for it.
+   * @param picker get ID values from this object.
+   */
+  private setId(picker: PickerInterface) {
+    this.gradientBox.setAttribute('id', picker.boxId);
+    this.picker.setAttribute('id', picker.pikerId);
+  }
+  /**
+   * Add class names to the created picker and it's box.
+   * @param picker get Class names from this object.
+   */
+  private setClass(picker: PickerInterface) {
+    this.gradientBox.setAttribute('class', picker.boxClass);
+    this.picker.setAttribute('class', picker.pickerClass);
+  }
+
+  /**
+   * set color picker to the starting position inside the parent rect.
+   * @param position x and y axis.
+   * @return x and y available values.
+   */
+  set setPickerPosition(position: {x: number, y: number}) {
+    this.pickerPos = this.boxCoords(position.x, position.y, this.gradientBox.getBoundingClientRect());
+    this.setPickerOffset(this.pickerPos);
+  }
+
+  /**
+   * Set picker coordinates inside the gradient box/
+   * @param x axis
+   * @param y axis
+   * @param rect parent element
+   * @return object of x y values
+   */
+  private boxCoords(x: number, y: number, rect: ClientRect) {
+      /* get max mouse position (based on min) */
+      const width = rect.width;
+      const height = rect.height;
+
+      /* out of box range */
+      if (x < 0) { x = 0; }
+      if (y < 0) { y = 0; }
+      if (x > width) { x = width - 1; }
+      if (y > height) { y = height - 1; }
+
+      return {x, y};
+  }
+
+  /**
+   * Set visual offsets for the picker
+   * @param coords coordinates of the x y axis
+   */
+  private setPickerOffset(coords: {x: number, y: number}) {
+      this.picker.style.left = (coords.x - this.pickerRadius) + 'px';
+      this.picker.style.top = (coords.y - this.pickerRadius) + 'px';
+  }
+
+  private setClickEvent(element: HTMLElement, event: EventListenerObject) {
+    element.addEventListener('click', event);
+  }
+}
---
+++ src/interface/PickerInterface.ts
@@ -0,0 +1,6 @@
+export interface PickerInterface {
+  boxId: string;
+  boxClass: string;
+  pikerId: string;
+  pickerClass: string;
+}
